1e4e10eae38bca74693565c5ae61beb0614bce88
samckp <sam.mca.liet@gmail.com>
2021-03-08 17:40:46 +0530

    code base added

---
+++ src/main/java/com/camel/multicast/CamelMulticastApplication.java
@@ -0,0 +1,13 @@
+package com.camel.multicast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMulticastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMulticastApplication.class, args);
+	}
+
+}
