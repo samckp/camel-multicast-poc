1e198f45ee45108e255f3e7fe2064b980c915f9a
samckp <sam.mca.liet@gmail.com>
2021-03-11 08:44:12 +0530

    codebase updated

--- src/main/java/com/camel/multicast/MultiRoute.java
+++ src/main/java/com/camel/multicast/MultiRoute.java
@@ -1,2 +1,22 @@
-package com.camel.multicast;public class MultiRoute {
+package com.camel.multicast;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MultiRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("{{inputFilePath}}")
+                .routeId("mutliRouteId")
+                .log(LoggingLevel.INFO, "Multicast Route Starting !!")
+                .multicast()
+                .to("{{toRoute}}")
+//                .to("")
+//                .to("")
+                ;
+    }
 }
